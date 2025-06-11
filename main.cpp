467c4b927922bfcb2e1200373b30787efc101b61
김혜인 <rjqnrdl7524@naver.com>
2025-06-11 11:22:34 +0900

    스프라이트 출력
    나무, 벌, 구름 등 게임 필요 스프라이트 그리기 작업

--- main.cpp
+++ main.cpp
@@ -2,14 +2,53 @@
 
 int main()
 {
+    // 창 설정
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Timber!");
    
-    sf::Texture textureBackground; // 배경화면 텍스쳐
-    textureBackground.loadFromFile("graphics/background.png"); // 배경화면 텍스쳐의 리소스 경로 설정
 
-    sf::Sprite spriteBackground; // 그리기용 객체
-    spriteBackground.setTexture(textureBackground); // 그리기용 객체의 텍스쳐 설정
+    // 텍스쳐 설정
+    sf::Texture textureBackground;                      
+    textureBackground.loadFromFile("graphics/background.png"); 
 
+    sf::Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+
+    sf::Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+
+    sf::Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+    
+
+    // 스프라이트
+    sf::Sprite spriteBackground; // 배경        
+    spriteBackground.setTexture(textureBackground);
+
+    sf::Sprite spriteCloud[3];  // 구름
+    for (int i = 0; i < 3; i++)
+    {
+        spriteCloud[i].setTexture(textureCloud);
+    }
+    
+    sf::Sprite spriteTree; // 나무
+    spriteTree.setTexture(textureTree);
+
+    sf::Sprite spriteBee;  // 벌
+    spriteBee.setTexture(textureBee);
+
+
+    // 위치 설정
+    spriteTree.setOrigin(textureTree.getSize().x * 0.5f, 0.0f);
+    spriteTree.setPosition(1920 * 0.5f, 0.0f);
+
+    for (int i = 0; i < 3; i++)
+    {
+        spriteCloud[i].setPosition(0.0f, textureCloud.getSize().y * i);
+    }
+
+    spriteBee.setPosition(30.0f, 800.0f);
+    // spriteTree.setPosition(1920 * 0.5f - textureTree.getSize().x * 0.5f, 0);
+    
 
     while (window.isOpen())
     {
@@ -21,7 +60,20 @@ int main()
         }
 
         window.clear();
-        window.draw(spriteBackground); // 배경화면 그리기
+
+
+        // Sprite 그리기
+        window.draw(spriteBackground);
+
+        for (int i = 0; i < 3; i++)
+        {
+            window.draw(spriteCloud[i]);
+        }
+        
+        window.draw(spriteTree);
+        
+        window.draw(spriteBee);
+
         window.display();
     }
 
